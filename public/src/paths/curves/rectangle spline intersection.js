4e4ad57d35ea854b502b4820a76a3a4f58a20e6e
Piotr Hanusiak <wacneg@gmail.com>
2020-05-13 13:40:13 +0200

    Update rectangle spline intersection.js
    Updated to work with the current 'drag' event definition.

--- public/src/paths/curves/rectangle spline intersection.js
+++ public/src/paths/curves/rectangle spline intersection.js
@@ -58,10 +58,10 @@ function create ()
     curve.getBounds(pathBounds);
     image.getBounds(spriteBounds);
 
-    this.input.on('DRAG_EVENT', function (event) {
-
-        event.gameObject.x = event.dragX;
-        event.gameObject.y = event.dragY;
+    this.input.on('drag', function (pointer, gameObject) {
+    
+        gameObject.x = pointer.x;
+        gameObject.y = pointer.y;
 
         image.getBounds(spriteBounds);
 
